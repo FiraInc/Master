57ff2d0928c46f14f9ec3b4cb6538ddf6b08fdb6
BuildTools <unconfigured@null.spigotmc.org>
2018-04-07 17:09:12 +0200

    First working release

---
+++ app/src/main/java/com/zostio/masterexample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zostio.masterexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zostio.master.Master;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
